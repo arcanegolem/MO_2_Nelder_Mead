d061f7c5b4e186641240d968ef6dc953220b8469
arcanegolem <miv738@gmail.com>
2023-02-18 16:28:28 +0300

    Some shitty code

--- nelder_mead.py
+++ nelder_mead.py
@@ -13,6 +13,10 @@ def guidedFunction(dot: Dot) -> float:
     return pow(dot.coord_x, 2) - (dot.coord_x * dot.coord_y) + (3 * pow(dot.coord_y, 2)) - dot.coord_x
 
 
+def methodFunction(dot: Dot) -> float:
+    return 2.8 * pow(dot.coord_y, 2) + 1.9 * dot.coord_x + 2.7 * pow(dot.coord_x, 2) + 1.6 - 1.9 * dot.coord_y
+
+
 class NelderMead:
     '''
     Класс для минимизации функции методом Нельдера-Мида
@@ -42,6 +46,7 @@ class NelderMead:
         self.          m = m
         self.    epsilon = epsilon
         self.       beta = beta
+        self.      gamma = gamma
         self.startingDot = start_dot
         self. targetFunc = targetFunction
 
@@ -52,17 +57,29 @@ class NelderMead:
         for res in self.dots:
             print(str(res) + "\t: ", str(self.dots[res]))
 
+        isDone = False
+
         iters = -1
 
-        min_val_dot, pre_max_val_dot, max_val_dot = self.getMinMaxPreMaxDotVal()
+        while not isDone:
+            iters += 1
 
-        cut_wc = self.findCutWeightCenter(max_val_dot[1])
+            min_val_dot, pre_max_val_dot, max_val_dot = self.getMinMaxPreMaxDotVal()
 
-        reflection = self.reflectDotAndResult(cut_wc, max_val_dot[1])
+            cut_wc = self.findCutWeightCenter(max_val_dot[1])
+            print("\nОтносительный центр тяжести расположен в точке")
+            print(str(cut_wc))
 
-        self.checkAndSwap(max_val_dot, reflection)
-        for result, dot in self.dots.items():
-                print(str(result) + "\t:", str(dot))
+            reflection = self.reflectDotAndResult(cut_wc, max_val_dot[1])
+            print("\nОтраженная вершина и результат в ней")
+            print(str(reflection[0]) + "\t:", str(reflection[1]))
+
+            isDone = self.checkAndSwap(max_val_dot, reflection, pre_max_val_dot, min_val_dot, cut_wc)
+            for res in self.dots:
+                print(str(res) + "\t: ", str(self.dots[res]))
+            
+            print(iters)
+
 
 
     def getMinMaxPreMaxDotVal(self):
@@ -83,19 +100,107 @@ class NelderMead:
         self.dots[self.targetFunc(newDot_2)] = newDot_2
 
     
-    def checkAndSwap(self, exclusion: tuple, reflection: tuple):
-        '''
-        Проверка отраженной точки и при успехе замена исключаемой, при неудаче редукция
-        '''
+    def checkAndSwap(self, exclusion: tuple, reflection: tuple, pre_max: tuple, least: tuple, cut_wc: Dot):
         if reflection[0] < exclusion[0]:
             self.dots[reflection[0]] = reflection[1]
             del self.dots[exclusion[0]]
+
+            print("\nРезультат функции в отраженной точке меньше наибольшего, начинаю растяжение")
+            return self.stretch(exclusion, reflection, pre_max, least, cut_wc)
         else:
-            self.compress()
+            print("\nРезультат функции в отраженной точке больше наибольшего, начинаю сжатие")
+            return self.compress(exclusion, reflection, pre_max, cut_wc)
 
 
-    def compress(self):
-        pass
+    def stretch(self, exclusion: tuple, reflection: tuple, pre_max: tuple, least: tuple, cut_wc: Dot) -> bool:
+        if reflection[0] < least[0]:
+            stretched_dot = cut_wc + ((reflection[1] - cut_wc) * self.beta)
+            stretched = (self.targetFunc(stretched_dot), stretched_dot)
+            print("\nТочка после растяжения:")
+            print(str(stretched[0]) + "\t:", str(stretched[1]))
+
+            if stretched[0] < reflection[0]:
+                print("\nРастяжение успешно!")
+                print(str(stretched[0]) + " < " + str(reflection[0]))
+
+                self.dots[stretched[0]] = stretched[1]
+                del self.dots[reflection[0]]
+
+                isDone = self.checkStopCriteria()
+                for result, dot in self.dots.items():
+                    print(str(result) + "\t:", str(dot))
+
+                return isDone
+            
+        print("\nРастяжение не удалось, перехожу к сжатию")
+        return self.compress(reflection, reflection, pre_max, cut_wc)
+
+
+    def checkStopCriteria(self) -> bool:
+        wc = self.findFullWeightCenter()
+
+        #omega = (sum_dots(list(map(lambda dot: (dot - wc) ** 2, self.dots.values()))) * (1/(self.n + 1))) ** 0.5
+        omega = pow(sum(list(map(lambda dot: pow(self.targetFunc(dot - wc), 2), self.dots.values()))), 0.5)
+        if omega < self.epsilon:
+            print("\nКритерий останова соблюден, завершение")
+            print(str(omega) + " < " + str(self.epsilon) + "\n")
+            return True
+        
+        print("\nКритерий останова НЕ соблюден, продолжаю")
+        print(str(omega) + " > " + str(self.epsilon) + "\n")
+        return False
+
+
+    def compress(self, exclusion: tuple, reflection:tuple, pre_max: tuple, cut_wc: Dot) -> bool:
+        compressed_dot = cut_wc + ((self.dots[exclusion[0]] - cut_wc) * self.gamma)
+        compressed = (self.targetFunc(compressed_dot), compressed_dot)
+        
+        if pre_max[0] < reflection[0] < exclusion[0]:
+            if compressed[0] < exclusion[0]:
+                print("\nСжатие прошло успешно")
+                print(str(compressed[0]) + " < " + str(exclusion[0]) + "\n")
+
+                self.dots[compressed[0]] = compressed[1]
+                del self.dots[exclusion[0]]
+
+                return self.checkStopCriteria()
+        
+        return self.reduce()
+
+
+    def reduce(self) -> bool:
+        minValDot = self.findMinValDot()
+        minDot = minValDot[1]
+
+        res_to_del = []
+
+        for result, dot in self.dots.copy().items():
+            if dot == minDot:
+                continue
+            else:
+                newDot = minDot + ((dot - minDot) * 0.5)
+                self.dots[self.targetFunc(newDot)] = newDot
+                res_to_del.append(result)
+
+        for key in res_to_del:
+            if key in self.dots.copy():
+                del self.dots[key]
+
+        return self.checkStopCriteria()
+
+
+    def findMinValDot(self):
+        '''
+        Нахождение точки с наименьшим результатом функции в ней
+        '''
+        min_result = sys.maxsize
+
+        for result, dot in self.dots.items():
+            if result < min_result:
+                min_result = result
+                min_dot    = dot
+
+        return (min_result, min_dot)
 
     
     def reflectDotAndResult(self, wc, dot) -> tuple:
@@ -119,6 +224,13 @@ class NelderMead:
         return (sum_dots(operated_dots)) * (1/self.n)
 
 
+    def findFullWeightCenter(self) -> Dot:
+        '''
+        Нахождение центра тяжести всего симплекса
+        '''
+        return (sum_dots(self.dots.values())) * (1/(self.n + 1))
+
+
     def calcDelta1(self) -> float:
         '''
         Получение первой дельты
@@ -133,4 +245,4 @@ class NelderMead:
         return ((sqrt(self.n + 1) + self.n - 1) / (self.n * sqrt(2))) * self.m
 
 
-nelder_mead = NelderMead(n = 2, start_dot = Dot(0, 0), m = 1, beta = 2.8, gamma = 0.4, epsilon = 0.1, targetFunction = guidedFunction)
+nelder_mead = NelderMead(n = 2, start_dot = Dot(0, 0), m = 0.75, beta = 1.85, gamma = 0.1, epsilon = 0.1, targetFunction = methodFunction)
